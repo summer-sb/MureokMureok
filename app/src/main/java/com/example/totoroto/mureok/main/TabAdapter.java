7692f4c1517032d863da2907a8dbc847220d2aed
Summer <>
2018-07-02 14:56:54 +0900

    PostListFragment : ViewModel 생성

--- app/src/main/java/com/example/totoroto/mureok/main/TabAdapter.java
+++ app/src/main/java/com/example/totoroto/mureok/main/TabAdapter.java
@@ -5,12 +5,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.example.totoroto.mureok.community.CommunityFragment;
+import com.example.totoroto.mureok.community.PostListFragment;
 import com.example.totoroto.mureok.list.ListFragment;
 import com.example.totoroto.mureok.manage.ManageFragment;
 
 
 public class TabAdapter extends FragmentStatePagerAdapter {
-    private final int numTab = 3;
+    private final int numTab = 4;
 
     public TabAdapter(FragmentManager fm) {
         super(fm);
@@ -25,6 +26,8 @@ public class TabAdapter extends FragmentStatePagerAdapter {
                 return ListFragment.newInstance();
             case 2:
                 return CommunityFragment.newInstance();
+            case 3:
+                return PostListFragment.newInstance();
             default:
                 return null;
         }
