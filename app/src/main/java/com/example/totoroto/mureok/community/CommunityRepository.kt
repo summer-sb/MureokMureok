7074ff63bb9cadf51b0c35b1218e6e6c9a6edffd
Summer <>
2018-07-03 14:14:23 +0900

    CommunityRepository, getPostList() 구현

--- app/src/main/java/com/example/totoroto/mureok/community/CommunityRepository.kt
+++ app/src/main/java/com/example/totoroto/mureok/community/CommunityRepository.kt
@@ -1,12 +1,17 @@
 package com.example.totoroto.mureok.community
 
 import com.example.totoroto.mureok.data.Post
-import java.util.ArrayList
+import io.reactivex.Observable
+import io.reactivex.Single
+import java.util.concurrent.Callable
+
 
 class CommunityRepository {
-    fun getPostList() : List<Post> {
-        val temp : List<Post> = listOf()
 
-        return temp
+    fun getPostList() : Single<List<Post>> {
+        val post = Post()
+
+        return Single.fromCallable(Callable { post.getPosts()})
+                .flatMap { posts -> Single.fromObservable(Observable.fromArray(posts))}
     }
 }
--- app/src/main/java/com/example/totoroto/mureok/data/Post.kt
+++ app/src/main/java/com/example/totoroto/mureok/data/Post.kt
@@ -1,3 +1,29 @@
 package com.example.totoroto.mureok.data
 
-class Post(val key: String, val imgProfile: String, val nickName: String, val date: String, val imgPicture: String, val contents: String, val typeCategory: Int, var numLike: Int)
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+class Post(val key: String = "", val imgProfile: String = "", val nickName: String = "", val date: String = "", val imgPicture: String = "",
+           val contents: String = "", val typeCategory: Int = 0, var numLike: Int = 0){
+
+    fun getPosts() : List<Post>{
+        val postList = mutableListOf<Post>()
+        val postRef = FirebaseDatabase.getInstance().reference.child("community").child("communityData")
+
+        postRef.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                postList.clear()
+                for (s in dataSnapshot.children) {
+                    val data = s.getValue(Post::class.java) ?: return
+                    postList.add(data)
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+
+        return postList
+    }
+}
