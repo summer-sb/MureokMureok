dd0bcc8fab442514148fc11ce9cff4e6fbf577b1
Summer <>
2018-07-04 10:00:08 +0900

    Post생성자 변경 및 CommunityRepository 비동기 처리

--- app/src/main/java/com/example/totoroto/mureok/community/CommunityRepository.kt
+++ app/src/main/java/com/example/totoroto/mureok/community/CommunityRepository.kt
@@ -1,17 +1,58 @@
 package com.example.totoroto.mureok.community
 
 import com.example.totoroto.mureok.data.Post
-import io.reactivex.Observable
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import io.reactivex.Single
 import java.util.concurrent.Callable
 
 
 class CommunityRepository {
+    interface PostCallback{
+        fun apply(posts : List<Post>)
+    }
+
+    var callback = object : PostCallback{
+        override fun apply(posts: List<Post>) {}
+    }
+
+    fun setPosts(p : PostCallback){
+        callback = p
+    }
+
+    fun getPosts(){
+        val postList = mutableListOf<Post>()
+        val postRef = FirebaseDatabase.getInstance().reference.child("community").child("communityData")
+
+        postRef.addListenerForSingleValueEvent(object : ValueEventListener{
+
+            override fun onDataChange(snapshot : DataSnapshot) {
+                for(s in snapshot.children){
+                    val data = s.getValue(Post::class.java) ?: return
+                    postList.add(data)
+                }
+                callback.apply(postList)
+            }
+
+            override fun onCancelled(err : DatabaseError?) {}
+        })
+    }
+
+
 
     fun getPostList() : Single<List<Post>> {
-        val post = Post()
+        var postList : List<Post> = listOf()
+        getPosts()
 
-        return Single.fromCallable(Callable { post.getPosts()})
-                .flatMap { posts -> Single.fromObservable(Observable.fromArray(posts))}
+        setPosts(object :PostCallback{
+            override fun apply(posts: List<Post>) {
+                postList = posts
+            }
+        })
+
+        return Single.fromCallable(Callable { postList })
     }
 }
+
