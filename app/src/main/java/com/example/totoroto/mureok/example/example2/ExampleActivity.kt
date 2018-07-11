33c48fe434f3abd8648e9e9f40a5e2296b5f0fbd
summer-sb <summer@hpcnt.com>
2018-07-11 11:18:12 +0900

    example1 내용, example2로 이동

--- app/src/main/java/com/example/totoroto/mureok/example/example2/ExampleActivity.kt
+++ app/src/main/java/com/example/totoroto/mureok/example/example2/ExampleActivity.kt
@@ -5,20 +5,15 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.totoroto.mureok.R
-import com.example.totoroto.mureok.R.id.text
-import com.example.totoroto.mureok.R.id.textView
-import com.google.android.gms.internal.tv
+import com.example.totoroto.mureok.example.example1.ApiServer
 import kotlinx.android.synthetic.main.activity_example.*
-import org.w3c.dom.Text
 import java.lang.ref.WeakReference
 import java.util.*
 
 /**
- * TODO 최종 결과가 [0, 1, 2, 3, 4]가 나오도록 수정
+ * 최종 결과가 [0, 1, 2, 3, 4]가 나오도록 수정
  * @author Changwoo Hong(chawoo@hpcnt.com)
  * @since 05 - 7월 - 2018
  */
@@ -99,6 +94,39 @@ class ExampleActivity: Activity() {
         Log.d(TAG, "${Thread.currentThread().name} print start")
         textView.text = list.toString()
         Log.d(TAG, "${Thread.currentThread().name} print end")
+    }
+
+    /* Example1 내용
+    * Thread, AsyncTask를 이용하여 textView에 ApiServer로부터 받아온 데이터를 출력하세요
+    */
+
+    private fun loadByThread() {
+        val handler = Handler()
+
+        Thread(Runnable {
+            val data = ApiServer.getData()
+
+            handler.post {
+                textView.text = data
+            }
+
+        }).start()
+    }
+
+    private fun loadByAsyncTask() {
+
+        Example1Task().execute()
+    }
+
+    internal inner class Example1Task : AsyncTask<Void, Void, String>() {
+
+        override fun doInBackground(vararg voids: Void): String = ApiServer.getData()
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+
+            textView.text = result
+        }
     }
 
     companion object {
