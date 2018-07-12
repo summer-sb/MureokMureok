1ae3959e57d3b92ec32003619bc62fe35bbac1f2
summer-sb <summer@hpcnt.com>
2018-07-12 12:31:54 +0900

    asyncTask를 이용해 데이터 출력

--- app/src/main/java/com/example/totoroto/mureok/example/example2/ExampleActivity.kt
+++ app/src/main/java/com/example/totoroto/mureok/example/example2/ExampleActivity.kt
@@ -1,12 +1,15 @@
 package com.example.totoroto.mureok.example.example2
 
 import android.app.Activity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.widget.TextView
 import com.example.totoroto.mureok.R
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_example.*
+import java.lang.ref.WeakReference
 
 /**
  *
@@ -27,6 +30,41 @@ class ExampleActivity: Activity() {
                                .subscribe{
                                    textView.text = it.toString()
                                }
+
+        val printTask = PrintTask(coffeeNameView, coffeePriceView)
+        printTask.execute()
+    }
+
+    private class PrintTask(coffeeNameView : TextView, coffeePriceView: TextView) : AsyncTask<Void, Void, List<String>>() {
+        private val coffeeNameView = WeakReference(coffeeNameView)
+        private val coffeePriceView = WeakReference(coffeePriceView)
+
+        override fun doInBackground(vararg params: Void?): List<String> {
+            val ids = ApiServer.getCoffeeIds()
+            val names = mutableMapOf<String, String>()
+            val prices = mutableMapOf<String, Int>()
+            val results : MutableList<String> = mutableListOf()
+
+            for(i in 0..1){
+                names[ids[i]] = ApiServer.getName(ids[i])
+                prices[ids[i]] = ApiServer.getPrice(ApiServer.getPriceCode(ids[i]))
+            }
+
+            for(i in 0..1) {
+                if(ids[i] == ApiServer.ID_AMERICANO) {
+                    results.add(names[ApiServer.ID_AMERICANO] ?: return emptyList())
+                    results.add(prices[ApiServer.ID_AMERICANO].toString())
+                }
+            }
+            return results
+        }
+        override fun onPostExecute(result: List<String>) {
+            super.onPostExecute(result)
+
+            coffeeNameView.get()?.text = result[0]
+            coffeePriceView.get()?.text = result[1]
+        }
+
     }
 
     override fun onDestroy() {
